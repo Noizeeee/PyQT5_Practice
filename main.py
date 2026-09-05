2b6ba81da039a349349d029428adfad94de08979
raine <raineaveryocampo@gmail.com>
2026-09-05 18:19:57 +0800

    Icon

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("First GUI hehi")
+        self.setGeometry(700, 300, 500, 500)
+        self.setWindowIcon(QIcon("ID.jpg"))
+
 
 def main():
     app = QApplication(sys.argv)
