5adf4af3f0a808481a8f6a4c02f8e9d6ed43e933
raine <raineaveryocampo@gmail.com>
2026-09-05 21:47:04 +0800

    Font Design

--- labels.py
+++ labels.py
@@ -1,13 +1,14 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         label = QLabel("Hello", self)
-        
+        label.setFont(QFont("Times New Roman", 20))
+
         self.setWindowIcon(QIcon('ID.jpg'))
         self.setWindowTitle("Random Label")
         self.setGeometry(700, 400, 500, 500)
